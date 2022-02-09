b2eff5b94ff7b0f2df1062880910e4b12dc9e3dc
HlafFool <sutthirak@nvc.ac.th>
2022-02-09 23:43:08 +0700

    Hot fix

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -13,91 +13,95 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final formKey = GlobalKey<FormState>();
   Profile profile = Profile();
-  final Future = FirebaseCoreWeb.registerWith;
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(builder: (context, snapshot) {
-      if (snapshot.hasError) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Error"),
-          ),
-          body: Center(
-            child: Text("${snapshot.error}"),
-          ),
-        );
-      }
-      if (snapshot.connectionState == ConnectionState.done) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("สร้างบัญชีผู้ใช้"),
-          ),
-          body: Container(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Form(
-                  key: formKey,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("อีเมล", style: TextStyle(fontSize: 20)),
-                        TextFormField(
-                          validator: MultiValidator([
-                            RequiredValidator(
-                                errorText: "กรุณาป้อนอีเมลด้วยครับ"),
-                            EmailValidator(errorText: "รูปแบบอีเมลไม่ถูกต้อง")
-                          ]),
-                          keyboardType: TextInputType.emailAddress,
-                          onSaved: (var email) {
-                            profile.email = email;
-                          },
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Error"),
+              ),
+              body: Center(
+                child: Text("${snapshot.error}"),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("สร้างบัญชีผู้ใช้"),
+              ),
+              body: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                      key: formKey,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("อีเมล", style: TextStyle(fontSize: 20)),
+                            TextFormField(
+                              validator: MultiValidator([
+                                RequiredValidator(
+                                    errorText: "กรุณาป้อนอีเมลด้วยครับ"),
+                                EmailValidator(
+                                    errorText: "รูปแบบอีเมลไม่ถูกต้อง")
+                              ]),
+                              keyboardType: TextInputType.emailAddress,
+                              onSaved: (var email) {
+                                profile.email = email;
+                              },
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text("รหัสผ่าน", style: TextStyle(fontSize: 20)),
+                            TextFormField(
+                              validator: RequiredValidator(
+                                  errorText: "กรุณาป้อนรหัสผ่านด้วยครับ"),
+                              obscureText: true,
+                              onSaved: (var password) {
+                                profile.password = password;
+                              },
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child: Text("ลงทะเบียน"),
+                                onPressed: () async {
+                                  if (formKey.currentState!.validate()) {
+                                    formKey.currentState!.save();
+                                    try {
+                                      await FirebaseAuth.instance
+                                          .createUserWithEmailAndPassword(
+                                              email: profile.email,
+                                              password: profile.password);
+                                      formKey.currentState?.reset();
+                                    } on FirebaseAuthException catch (e) {
+                                      print(e.message);
+                                    }
+                                  }
+                                },
+                              ),
+                            )
+                          ],
                         ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text("รหัสผ่าน", style: TextStyle(fontSize: 20)),
-                        TextFormField(
-                          validator: RequiredValidator(
-                              errorText: "กรุณาป้อนรหัสผ่านด้วยครับ"),
-                          obscureText: true,
-                          onSaved: (var password) {
-                            profile.password = password;
-                          },
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            child: Text("ลงทะเบียน"),
-                            onPressed: () async {
-                              if (formKey.currentState!.validate()) {
-                                formKey.currentState!.save();
-                                try {
-                                  await FirebaseAuth.instance
-                                      .createUserWithEmailAndPassword(
-                                          email: profile.email,
-                                          password: profile.password);
-                                  formKey.currentState?.reset();
-                                } on FirebaseAuthException catch (e) {
-                                  print(e.message);
-                                }
-                              }
-                            },
-                          ),
-                        )
-                      ],
-                    ),
-                  )),
+                      )),
+                ),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        );
-      }
-      return Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    });
+          );
+        });
   }
 }
 
